044a51b4d51ca6e8cf29632a2c5546f863b8c26f
Naveen Setty <naveenkumarpg@gmail.com>
2024-06-17 18:03:37 +0100

    Removed static site.

--- staticsite/pages/api/articles/[id].js
+++
@@ -1,6 +0,0 @@
-import { articles } from '../../../data/posts';
-
-export default (req, res) => {
-  const { id } = req.query;
-  res.status(200).json(articles[id]);
-};
--- staticsite/pages/api/articles/index.js
+++
@@ -1,5 +0,0 @@
-import { articles } from '../../../data/posts';
-
-export default (req, res) => {
-  res.status(200).json(articles);
-};
